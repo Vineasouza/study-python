b61afe67c794bf4a79d404849f1e17c81ef48056
Vinicius Souza <vineasouza33@hotmail.com>
2019-12-10 20:43:46 -0300

    add aula de variaveis

---
+++ intro/variaveis.py
@@ -0,0 +1,21 @@
+#-*- coding: utf-8 -*-
+
+message = "Hello World"
+print (message)
+
+
+#variavel inteira
+var1 = 1
+print (var1)
+
+#variavel float
+var2 = 1.1
+print (var2)
+
+#variavel string
+var3 = "string"
+print (var3)
+
+#variavel booleana
+var4 = True
+print (var4)
