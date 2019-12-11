4e312c0d9e39b7432a547cb01995e1206b741809
Vinicius Souza <vineasouza33@hotmail.com>
2019-12-10 21:05:36 -0300

    add aula operadores logicos e relacionais

---
+++ intro/oprelog.py
@@ -0,0 +1,27 @@
+#operadores relacionais
+x = 2
+y = 3
+
+print("IGUAL:")
+print(x == y)
+print("MAIOR IGUAL:")
+print(x >= y)
+print("MAIOR:")
+print(x > y)
+print("MENOR IGUAL:")
+print(x <= y)
+print("MENOR:")
+print(x < y)
+
+
+#operadores logicos
+x = 2
+y = 2
+z = 3
+
+print("AND:")
+print(x == y and x == z)
+print("OR:")
+print(x == y or x == z)
+
+
