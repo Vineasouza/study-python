86782abda0d95d685948b4d0ca3ad4ada1912b4f
Vinicius Souza <vineasouza33@hotmail.com>
2019-12-12 01:02:33 -0300

    add aula de estrutura for

---
+++ intro/repeatloops.py
@@ -0,0 +1,26 @@
+#comando while
+x = 0
+
+while x < 10:
+    print(x)
+    x += 1
+
+#comando for
+lista1 = [1,2,3,4,5]
+lista2 = ["hello","world","!"]
+
+for i in lista1:
+    print(i)
+
+for i in lista2:
+    print(i)
+
+#comando for...range
+for i in range(10):
+    print(i)
+
+for i in range(10,20):
+    print(i)
+
+for i in range(10,20,2):
+    print(i)
