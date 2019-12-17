e7f7938f667dffec892930717a8186da030906e6
Vinicius Souza <vineasouza33@hotmail.com>
2019-12-17 01:34:38 -0300

    add aula random numbers

---
+++ intro/randomnum.py
@@ -0,0 +1,15 @@
+import random
+
+#gera numero aleatorio no intervalo definido
+num = random.randint(0,10)
+print(num)
+
+#escolhe um numero aleatorio na lista
+lista= [6, 45, 9]
+num = random.choice(lista)
+print(num)
+
+#shuffle
+print("Lista original : ",lista)
+random.shuffle(lista)
+print("Shuffle : ", lista)
