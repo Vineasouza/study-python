6315b528bd60ce9ee1a36b634dadf51d4047a3f4
Vinicius Souza <vineasouza33@hotmail.com>
2019-12-17 01:57:06 -0300

    add resolucao exercicio 1

---
+++ intro/exercicios/ex1.py
@@ -0,0 +1,9 @@
+#Faca um programa que receba a idade do usuario
+#e diga se ele e maior ou menor de idade.
+
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("Voce e maior de idade")
+else:
+    print("Voce e menor de idade")
