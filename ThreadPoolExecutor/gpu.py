d5e9354348472b355e94f1ba5ec017aeb69a09bc
Vineasouza <vineasouza33@hotmail.com>
2022-03-26 21:30:08 -0300

    test using GPU processing

---
+++ ThreadPoolExecutor/gpu.py
@@ -0,0 +1,21 @@
+from numba import jit
+import numpy as np
+from timeit import default_timer as timer
+# To run on CPU
+def func(a):
+    for i in range(10000000):
+        a[i]+= 1
+# To run on GPU
+@jit(nopython=True, parallel=True)
+def func2(x):
+    return x+1
+if __name__=="__main__":
+    n = 10000000
+    a = np.ones(n, dtype = np.float64)
+    start = timer()
+    func(a)
+    print("without GPU:", timer()-start)
+    start = timer()
+    func2(a)
+    #numba.cuda.profile_stop()
+    print("with GPU:", timer()-start)
